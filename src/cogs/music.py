f3b1fb0739bfc84668341ec779144b1f08b49739
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2025-08-31 19:41:52 -0300

    adição de arquivo cookies para login no linux

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -17,7 +17,8 @@ YTDL_OPTIONS = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'cookiefile': 'cookies.txt'
 }
 
 FFMPEG_OPTIONS = {
